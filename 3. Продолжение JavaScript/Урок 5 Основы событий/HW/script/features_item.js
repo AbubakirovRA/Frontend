e13391ff9910ef9626ff7701d91d1d8c32d913d1
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-05-12 07:27:53 +0500

    Add files via upload

---
+++ 3. Продолжение JavaScript/Урок 5 Основы событий/HW/script/features_item.js
@@ -0,0 +1,61 @@
+fetch('./json/featured_items.json')
+  .then(response => response.json())
+  .then(data => {
+    const featuredItems = data.featuredItems;
+    // Получение ссылки на блок товаров
+    const productBoxContent = document.querySelector('.product-box__content');
+
+    // Создание карточек товаров на основе данных из JSON
+    featuredItems.forEach(item => {
+      const productCard = document.createElement('div');
+      productCard.classList.add('product');
+
+      const productImg = document.createElement('div');
+      productImg.classList.add('product__img', 'add_cart');
+
+      const imgLink = document.createElement('a');
+      imgLink.href = '#';
+      imgLink.classList.add('img__link');
+
+      const img = document.createElement('img');
+      img.src = item.image;
+      img.alt = item.title;
+
+      imgLink.appendChild(img);
+      productImg.appendChild(imgLink);
+
+      const productContent = document.createElement('div');
+      productContent.classList.add('product__content');
+
+      const heading = document.createElement('p');
+      heading.classList.add('product__heading');
+      heading.textContent = item.title;
+
+      const text = document.createElement('p');
+      text.classList.add('product__text');
+      text.textContent = item.description;
+
+      const price = document.createElement('p');
+      price.classList.add('product__price');
+      price.textContent = item.price;
+
+      productContent.appendChild(heading);
+      productContent.appendChild(text);
+      productContent.appendChild(price);
+
+      productCard.appendChild(productImg);
+      productCard.appendChild(productContent);
+
+      productBoxContent.appendChild(productCard);
+    });
+  })
+
+// Обработка данных после загрузки JSON-файла
+function handleJSONLoad() {
+  if (typeof featuredItems !== 'undefined') {
+    handleData(featuredItems);
+  }
+}
+
+// Событие загрузки JSON-файла
+window.addEventListener('DOMContentLoaded', handleJSONLoad);
