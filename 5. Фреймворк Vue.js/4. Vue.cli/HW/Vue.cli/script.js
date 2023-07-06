5259f8442c20840d877fe60122de96da82afaf13
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-07-06 23:14:38 +0500

    Add files via upload

--- 5. Фреймворк Vue.js/4. Vue.cli/HW/Vue.cli/script.js
+++ 5. Фреймворк Vue.js/4. Vue.cli/HW/Vue.cli/script.js
@@ -1,8 +1,65 @@
-import Vue from 'vue';
-import App from './App.vue';
+Vue.component('tags', {
+  data() {
+    return {
+      selectedTag: null,
+      tags: ['Tag1', 'Tag2', 'Tag3']
+    };
+  },
+  methods: {
+    selectTag(tag) {
+      this.selectedTag = tag;
+      this.$emit('select-tag', tag);
+    }
+  },
+  template: `
+    <div>
+      <h2>Tags</h2>
+      <ul>
+        <li v-for="tag in tags" :key="tag" @click="selectTag(tag)" :class="{ 'active': selectedTag === tag }">{{ tag }}</li>
+      </ul>
+    </div>
+  `
+});
 
-Vue.config.productionTip = false;
+Vue.component('article-list', {
+  props: ['articles'],
+  template: `
+    <div>
+      <h2>Articles</h2>
+      <ul>
+        <li v-for="article in articles" :key="article.title">
+          <h3>{{ article.title }}</h3>
+          <p>{{ article.content }}</p>
+        </li>
+      </ul>
+    </div>
+  `
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  el: '#app',
+  data() {
+    return {
+      articles: [
+        { title: 'Article 1', content: 'Content 1', tags: ['Tag1'] },
+        { title: 'Article 2', content: 'Content 2', tags: ['Tag2'] },
+        { title: 'Article 3', content: 'Content 3', tags: ['Tag3'] }
+      ],
+      selectedTag: null
+    };
+  },
+  methods: {
+    updateSelectedTag(tag) {
+      this.selectedTag = tag;
+    }
+  },
+  computed: {
+    sortedArticles() {
+      if (this.selectedTag) {
+        return this.articles.filter(article => article.tags.includes(this.selectedTag));
+      } else {
+        return this.articles;
+      }
+    }
+  }
+});
