3ad53cc1e54f15131414c71921a1d7806749e9f9
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-07-10 21:55:21 +0500

    Create ProductDetails.vue

---
+++ 5. Фреймворк Vue.js/Урок 5. Встроенные директивы и работа с данными компонента/HW/ProductDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{ product.name }}</h2>
+    <p>Price: {{ formattedPrice }}</p>
+    <p>Status: {{ product.available ? 'Available' : 'Out of stock' }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formattedPrice() {
+      // Предполагается, что в объекте "product" есть свойство "price"
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(this.product.price);
+
+      return formatted;
+    }
+  }
+};
+</script>
