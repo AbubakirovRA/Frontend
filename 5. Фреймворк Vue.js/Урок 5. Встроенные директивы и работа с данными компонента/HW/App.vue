0f92d3bc3514986ee34192e0920cc7b4d25a3368
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-07-10 21:55:48 +0500

    Create App.vue

---
+++ 5. Фреймворк Vue.js/Урок 5. Встроенные директивы и работа с данными компонента/HW/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <ProductDetails :product="productData" />
+  </div>
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+  },
+  data() {
+    return {
+      productData: {
+        name: 'Example Product',
+        price: 99.99,
+        available: true
+      }
+    };
+  }
+};
+</script>
