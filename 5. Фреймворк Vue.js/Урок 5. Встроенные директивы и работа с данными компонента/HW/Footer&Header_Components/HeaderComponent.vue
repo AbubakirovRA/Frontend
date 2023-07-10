1737de5bcb6d5df3051b32af1858ae18c28d07d5
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-07-10 22:53:32 +0500

    Create HeaderComponent.vue

---
+++ 5. Фреймворк Vue.js/Урок 5. Встроенные директивы и работа с данными компонента/HW/Footer&Header_Components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+      <h1>Мой Интернет-Магазин</h1>
+      <!-- Дополнительный контент шапки -->
+    </header>
+  </template>
+
+  <style scoped>
+  header {
+    background-color: #f5f5f5;
+    padding: 20px;
+    /* Дополнительные стили шапки */
+  }
+  </style>
