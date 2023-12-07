4e792a007be6e302a9903ed7b94e73a1096dc442
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-12-07 15:17:44 +0500

    Update app.js

--- 8. Основы Node.js/4. Написание API с применением REST/HW/app.js
+++ 8. Основы Node.js/4. Написание API с применением REST/HW/app.js
@@ -1,4 +1,4 @@
-// npm install express fs
+// npm install express fs - установка express
 
 const express = require('express');
 const fs = require('fs/promises');
@@ -11,111 +11,158 @@ app.use(express.json());
 
 // Middleware для проверки существования файла с данными
 const checkDataFile = async (req, res, next) => {
-  try {
-    await fs.access(DATA_FILE);
-    next();
-  } catch (error) {
-    // Если файл не существует, создаем его с пустым массивом
-    await fs.writeFile(DATA_FILE, '[]');
-    next();
-  }
+    try {
+        await fs.access(DATA_FILE);
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+
+        if (!data.trim()) {
+            await fs.writeFile(DATA_FILE, '{"users": []}');
+        }
+
+        next();
+    } catch (error) {
+        console.error('Error in checkDataFile:', error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 // Получение всех пользователей
 app.get('/users', checkDataFile, async (req, res) => {
-  try {
-    const data = await fs.readFile(DATA_FILE, 'utf-8');
-    const users = JSON.parse(data);
-    res.json(users);
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
+    try {
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+        const jsonData = JSON.parse(data);
+
+        if (!jsonData.hasOwnProperty('users') || !Array.isArray(jsonData.users)) {
+            return res.status(500).send('Internal Server Error: Invalid data format');
+        }
+
+        const users = jsonData.users;
+        res.json(users);
+    } catch (error) {
+        console.error('Error in GET /users:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 // Получение пользователя по ID
 app.get('/users/:id', checkDataFile, async (req, res) => {
-  const userId = parseInt(req.params.id);
+    const userId = parseInt(req.params.id);
 
-  try {
-    const data = await fs.readFile(DATA_FILE, 'utf-8');
-    const users = JSON.parse(data);
-    const user = users.find((u) => u.id === userId);
+    try {
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+        const jsonData = JSON.parse(data);
+        const users = jsonData.users;
+        const user = users.find((u) => u.id === userId);
 
-    if (user) {
-      res.json(user);
-    } else {
-      res.status(404).send('User not found');
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
 });
 
 // Создание нового пользователя
 app.post('/users', checkDataFile, async (req, res) => {
-  try {
-    const data = await fs.readFile(DATA_FILE, 'utf-8');
-    const users = JSON.parse(data);
-    const newUser = req.body;
+    try {
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+        const usersData = JSON.parse(data);
 
-    // Генерация уникального ID (просто для примера)
-    newUser.id = users.length + 1;
+        // Проверка, что в теле запроса есть массив с ключом "users"
+        if (!req.body.hasOwnProperty('users') || !Array.isArray(req.body.users)) {
+            return res.status(400).send('Bad Request: Missing or invalid "users" field');
+        }
 
-    users.push(newUser);
-    await fs.writeFile(DATA_FILE, JSON.stringify(users, null, 2));
+        const newUsers = req.body.users;
 
-    res.json(newUser);
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
+        // Проверка обязательных полей в каждом пользователе
+        const requiredFields = ['name', 'age'];
+
+        for (const newUser of newUsers) {
+            // Генерация уникального ID (просто для примера)
+            newUser.id = usersData.users.length + 1;
+
+            for (const field of requiredFields) {
+                if (!newUser.hasOwnProperty(field)) {
+                    return res.status(400).send(`Bad Request: Missing field "${field}"`);
+                }
+            }
+        }
+
+        // Добавление новых пользователей в существующий массив
+        usersData.users.push(...newUsers);
+
+        await fs.writeFile(DATA_FILE, JSON.stringify(usersData, null, 2));
+
+        res.json({ users: newUsers });
+    } catch (error) {
+        console.error('Error in POST /users:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-// Обновление пользователя
 app.put('/users/:id', checkDataFile, async (req, res) => {
-  const userId = parseInt(req.params.id);
+    const userId = parseInt(req.params.id);
 
-  try {
-    const data = await fs.readFile(DATA_FILE, 'utf-8');
-    const users = JSON.parse(data);
-    const updatedUser = req.body;
+    try {
+        // Чтение данных из файла
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+        const jsonData = JSON.parse(data);
 
-    const index = users.findIndex((u) => u.id === userId);
+        // Поиск пользователя по ID
+        const userIndex = jsonData.users.findIndex((u) => u.id === userId);
 
-    if (index !== -1) {
-      users[index] = { ...users[index], ...updatedUser };
-      await fs.writeFile(DATA_FILE, JSON.stringify(users, null, 2));
-      res.json(users[index]);
-    } else {
-      res.status(404).send('User not found');
+        if (userIndex !== -1) {
+            // Проверка наличия корректных данных в теле запроса
+            if (!req.body.hasOwnProperty('name') || !req.body.hasOwnProperty('age')) {
+                return res.status(400).send('Bad Request: Missing name or age field');
+            }
+
+            // Обновляем только обязательные поля
+            jsonData.users[userIndex].name = req.body.name;
+            jsonData.users[userIndex].age = req.body.age;
+
+            // Запись обновленных данных в файл
+            await fs.writeFile(DATA_FILE, JSON.stringify(jsonData, null, 2));
+
+            // Отправка обновленных данных в ответ
+            res.json(jsonData.users[userIndex]);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
 });
 
 // Удаление пользователя
 app.delete('/users/:id', checkDataFile, async (req, res) => {
-  const userId = parseInt(req.params.id);
+    const userId = parseInt(req.params.id);
 
-  try {
-    const data = await fs.readFile(DATA_FILE, 'utf-8');
-    let users = JSON.parse(data);
+    try {
+        const data = await fs.readFile(DATA_FILE, 'utf-8');
+        const jsonData = JSON.parse(data);
+        let users = jsonData.users;
 
-    const index = users.findIndex((u) => u.id === userId);
+        const index = users.findIndex((u) => u.id === userId);
 
-    if (index !== -1) {
-      const deletedUser = users.splice(index, 1)[0];
-      await fs.writeFile(DATA_FILE, JSON.stringify(users, null, 2));
-      res.json(deletedUser);
-    } else {
-      res.status(404).send('User not found');
+        if (index !== -1) {
+            const deletedUser = users.splice(index, 1)[0];
+            await fs.writeFile(DATA_FILE, JSON.stringify(users, null, 2));
+            res.json(deletedUser);
+        } else {
+            console.log(`User with id ${userId} not found.`);
+            res.status(404).send('User not found');
+        }
+    } catch (error) {
+        console.error('Error in DELETE /users/:id:', error);
+        res.status(500).send('Internal Server Error');
     }
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
 });
 
 // Запуск сервера
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
